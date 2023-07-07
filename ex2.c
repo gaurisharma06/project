9cf069940f544fafacf50f63a2c2995523e7908b
gaurisharma06 <gs1830993@gmail.com>
2023-07-07 19:53:28 +0530

    printf added

--- ex2.c
+++ ex2.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h> 
-#include<time.h>   // for using time 
-#include<unistd.h>   // for delay function sleep()
+#include<stdlib.h> // this header file contain function  rand(), srand().
+#include<time.h>   // this header file 
+#include<unistd.h> 
 int main()
 {
     int num ;    // decare a variable  that is use in loop 
-      srand(time(NULL)); // seed the random number genrerator  with  current time.  
+      srand(time(NULL)); // seed the random number genrerator with current time  
 
       while(1)   // Infinite loop
       {
           num = rand()%100 + 1; // use rand() function to get the random number  from  1 to 100 
-        printf (" %d\n ", num);  // print the generated random number 
+        printf ("  this is your random number:%d\n ", num);  // print the generated random number 
      sleep(1);  // time delay 1 sec 
-        
+      
     }
     return 0;
 }
